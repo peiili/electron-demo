06e88505c088defc8475785e8725f86580308230
peiili <1017935195@qq.com>
2020-07-12 23:32:05 +0800

    frist commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const {app,BrowserWindow}  = require('electron')
+// const event  = require('./event')
+function createWindow(){
+  let win = new BrowserWindow({
+    width:800,
+    height:600,
+    alwaysOnTop:true,
+    webPreferences:{
+      // 浏览器解析require（）
+      nodeIntegration:true
+    }
+  })
+  win.loadFile('./index.html')
+  // win.loadURL('https://www.google.com')
+  win.webContents.openDevTools()
+}
+app.whenReady().then(createWindow)
+app.on('window-all-closed',()=>{
+  if(process.platform!=='darwin'){
+    app.quit()
+  }
+})
+app.on('active',()=>{
+  if(BrowserWindow.getAllWindows().length===0){
+    createWindow()
+  }
+})
